a0af153e8c824fe3ec346f84112ac69f9159e787
Jenath <201908660@post.au.dk>
2021-04-09 14:20:40 +0200

    lys

--- MainCode/src/Transmitter.py
+++ MainCode/src/Transmitter.py
@@ -43,8 +43,8 @@ def SensorValues():
     IPC.ipcSend(config['pipes']['multiGET'], "GET")
     multi = IPC.ipcRead1(config['pipes']['multiVALUE'])
     multi = eval(multi)
-    voltage = multi[0]
-    current = multi[1]
+    voltage = multi[0]+0.001
+    current = multi[1]+0.001
     power = multi[2]
     print("Got voltage: " + str(voltage))
     print("Got current: " + str(current))
