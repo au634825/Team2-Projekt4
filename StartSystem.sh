3458a7838a3298d1dc818f4df53a8823c286f59b
root <root@beaglebone.localdomain>
2021-04-22 08:58:43 +0200

    files

--- StartSystem.sh
+++ StartSystem.sh
@@ -3,4 +3,9 @@
 # Team2 Project 4 - start up script.
 
 echo "Starting Receiver.."
-python3 ./MainCode/src/Receiver.py
+python3 ./MainCode/src/Receiver.py &
+echo "Receiver started"
+
+echo "Starting Transmitter.."
+python3 ./MainCode/src/Transmitter.py &
+echo "Transmitter started"
